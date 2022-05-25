c6e4a764dd4c03a10df0f16e845331679a275343
Sione Valita <84358346+sionevalita@users.noreply.github.com>
2022-05-25 21:15:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#*******************starting point***********#
+if __name__ == "__main__":
+  root = Tk()#parent window
+  root.title("MRGS Maths Quiz")
+  quizStarter_object = QuizStarter(root)
+  root.mainloop()#window doesn't dissapear
